1ff646d3981b1906c731443d9dfb1a8ecbae1e4e
Andrei Cristea <cristea.r.andrei@gmail.com>
2025-08-06 13:38:40 +0300

    Disabled file upload

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -117,7 +117,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, disabled = f
             rows={1}
             className="message-input"
           />
-          <button
+          {/* <button
             type="button"
             onClick={() => fileInputRef.current?.click()}
             disabled={disabled || isUploading}
@@ -127,7 +127,7 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSendMessage, disabled = f
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66L9.64 16.2a2 2 0 0 1-2.83-2.83l8.49-8.49" />
             </svg>
-          </button>
+          </button> */}
           <button
             type="submit"
             disabled={!message.trim() || disabled || isUploading}
